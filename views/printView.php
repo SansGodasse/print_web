7114245cd16bb9053a89a538eaa3053568b7781f
Nicolas <nicolas.renvoise.dev@gmail.com>
2019-04-03 14:08:05 +0100

    Make erase button not printable

--- views/printView.php
+++ views/printView.php
@@ -4,7 +4,7 @@ ob_start();
 <label class="not-printable" for="paste-area">Copier/coller ici avant d'imprimer</label><br>
 <textarea class="not-printable" name="paste-area" id="paste-area" cols="30" rows="10"></textarea>
 
-<button type="button" onclick="erase();">Effacer</button>
+<button class="not-printable" type="button" onclick="erase();">Effacer</button>
 
 <div id="printable-area"></div>
 
